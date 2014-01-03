909f9cce0c7cae6a0ae5b88702996ec22944f1f0
webrian <adrian.weber@cde.unibe.ch>
2014-01-03 12:58:50 +0100

    Extract EVI and QUAL bands

--- extract-bands.sh
+++ extract-bands.sh
@@ -20,17 +20,22 @@
 #
 
 # Check if VITS_DATA_PATH is set
-${VITS_DATA_PATH:?"Variable VITS_DATA_PATH is not set."}
-
-if [ -d $HOME/bin ]; then
-    export PATH=$HOME/bin:$PATH
+if [ ! -d "$VITS_DATA_PATH" ]; then
+    echo "Variable VITS_DATA_PATH is not set or not a directory."
+    exit 1
 fi
 
 inputPath="$VITS_DATA_PATH/MODIS/MOLT"
-outputPath="$VITS_DATA_PATH/MODIS/processed/NDVI"
+# Path to processed NDVI raster bands
+ndviOutputPath="$VITS_DATA_PATH/MODIS/processed/NDVI"
+# Path to processed EVI raster bands
+eviOutputPath="$VITS_DATA_PATH/MODIS/processed/EVI"
+# Path to Quality raster bands
+qualOutputPath="$VITS_DATA_PATH/MODIS/processed/QUAL" 
 
 product="MOD13Q1.005"
 
+# List of MODIS tiles
 tiles="h16v08 h17v08 h18v04 h20v09 h21v07 h21v08 h21v09 h21v10 h22v07 h22v08 h22v09 h27v06 h27v07 h27v08 h28v06 h28v07 h28v08"
 
 for timeDir in `find $inputPath/$product/* -type d`
@@ -38,22 +43,68 @@ do
     # Loop all tiles
     for tile in $tiles
     do
-        # Create an output tile directory if not yet existing
-        if [ ! -d "$outputPath/$tile" ]; then
-            mkdir "$outputPath/$tile"
-        fi
         hdfFileName=`ls "$timeDir/" | grep "${tile}.*hdf$"`
+        # Handle NDVI raster band first
+        # Create an output tile directory if not yet existing
+        if [ ! -d "$ndviOutputPath/$tile" ]; then
+            mkdir "$ndviOutputPath/$tile"
+        fi
         if [ -f "$timeDir/$hdfFileName" ]; then
-            subdatasetName=`gdalinfo $timeDir/$hdfFileName | grep -o SUBDATASET_1_NAME=.* | sed 's/SUBDATASET_1_NAME=//g'`
-            if [ ! -d "$outputPath/$tile" ]; then
-                mkdir "$outputPath/$tile"
+            ndviSubdatasetName=`gdalinfo $timeDir/$hdfFileName | grep -o SUBDATASET_1_NAME=.* | sed 's/SUBDATASET_1_NAME=//g'`
+            if [ ! "$ndviSubdatasetName" == '' ]; then
+                if [ ! -d "$ndviOutputPath/$tile" ]; then
+                    mkdir "$ndviOutputPath/$tile"
+                fi
+                ndviOutputFileName=NDVI_`echo "'$ndviSubdatasetName'" | grep -o "MOD13Q1\.[0-9a-zA-Z\.]*\.hdf" | sed 's/\.hdf$/\.tif/g'`
+                if [ ! -f "$ndviOutputPath/$tile/$ndviOutputFileName" ]; then
+                    gdal_translate "$ndviSubdatasetName" "$ndviOutputPath/$tile/$ndviOutputFileName"
+                fi
+            else
+                echo "$timeDir/$hdfFileName is not a valid GDAL raster"
+                rm -rv "$timeDir/$hdfFileName"
+                rm -rv "$timeDir/$hdfFileName.xml"
             fi
-            #echo "$subdatasetName"
-            outputFileName=NDVI_`echo "'$subdatasetName'" | grep -o "MOD13Q1\.[0-9a-zA-Z\.]*\.hdf" | sed 's/\.hdf$/\.tif/g'`
-            #echo $outputPath/$tile/$outputFileName
-            #echo $subdatasetName
-            if [ ! -f "$outputPath/$tile/$outputFileName" ]; then
-                gdal_translate "$subdatasetName" "$outputPath/$tile/$outputFileName"
+        fi
+        # Handle EVI raster band
+        # Create an output tile directory if not yet existing
+        if [ ! -d "$eviOutputPath/$tile" ]; then
+            mkdir "$eviOutputPath/$tile"
+        fi
+        if [ -f "$timeDir/$hdfFileName" ]; then
+            eviSubdatasetName=`gdalinfo $timeDir/$hdfFileName | grep -o SUBDATASET_2_NAME=.* | sed 's/SUBDATASET_2_NAME=//g'`
+            if [ ! "$eviSubdatasetName" == '' ]; then
+                if [ ! -d "$eviOutputPath/$tile" ]; then
+                    mkdir "$eviOutputPath/$tile"
+                fi
+                eviOutputFileName=EVI_`echo "'$eviSubdatasetName'" | grep -o "MOD13Q1\.[0-9a-zA-Z\.]*\.hdf" | sed 's/\.hdf$/\.tif/g'`
+                if [ ! -f "$eviOutputPath/$tile/$eviOutputFileName" ]; then
+                    gdal_translate "$eviSubdatasetName" "$eviOutputPath/$tile/$eviOutputFileName"
+                fi
+            else
+                echo "$timeDir/$hdfFileName is not a valid GDAL raster"
+                rm -rv "$timeDir/$hdfFileName"
+                rm -rv "$timeDir/$hdfFileName.xml"
+            fi
+        fi
+        # Handle QUAL raster band
+        # Create an output tile directory if not yet existing
+        if [ ! -d "$qualOutputPath/$tile" ]; then
+            mkdir "$qualOutputPath/$tile"
+        fi
+        if [ -f "$timeDir/$hdfFileName" ]; then
+            qualSubdatasetName=`gdalinfo $timeDir/$hdfFileName | grep -o SUBDATASET_3_NAME=.* | sed 's/SUBDATASET_3_NAME=//g'`
+            if [ ! "$qualSubdatasetName" == '' ]; then
+                if [ ! -d "$qualOutputPath/$tile" ]; then
+                    mkdir "$qualOutputPath/$tile"
+                fi
+                qualOutputFileName=QUAL_`echo "'$qualSubdatasetName'" | grep -o "MOD13Q1\.[0-9a-zA-Z\.]*\.hdf" | sed 's/\.hdf$/\.tif/g'`
+                if [ ! -f "$qualOutputPath/$tile/$qualOutputFileName" ]; then
+                    gdal_translate "$qualSubdatasetName" "$qualOutputPath/$tile/$qualOutputFileName"
+                fi
+            else
+                echo "$timeDir/$hdfFileName is not a valid GDAL raster"
+                rm -rv "$timeDir/$hdfFileName"
+                rm -rv "$timeDir/$hdfFileName.xml"
             fi
         fi
     done
