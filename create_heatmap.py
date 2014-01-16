4d6c2e438bd6f667ee4933811f04f9c85cd2f058
webrian <adrian.weber@cde.unibe.ch>
2014-01-16 11:49:25 +0100

    Add comments

--- create_heatmap.py
+++ create_heatmap.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python
+#
+# Script create a heatmap from accessed locations
+# Copyright (C) 2014 Adrian Weber
+# Centre for Development and Environment, University of Bern
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
-# see also:
+
+# Read more about about creating heatmaps with matplotlib:
 # http://stackoverflow.com/questions/2369492/generate-a-heatmap-in-matplotlib-using-a-scatter-data-set
 
 import sys
 
+# Import matplotlib add use the AGG backend renderer to enable this script
+# in a headless server environment
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import os
-import osgeo.ogr
+try:
+    import ogr
+except ImportError:
+    import osgeo.ogr as ogr
 try:
     import osr
 except ImportError:
@@ -37,13 +63,13 @@ def main(argv=None):
     outputWidth = 4000
 
     # get the shape file driver
-    shpDriver = osgeo.ogr.GetDriverByName('VRT')
+    vrtDriver = ogr.GetDriverByName('VRT')
 
     # Open shape file with points
-    shpFile = os.path.abspath(argv[1])
-    dataSource = shpDriver.Open(shpFile, 0)
+    vrtFile = os.path.abspath(argv[1])
+    dataSource = vrtDriver.Open(vrtFile, 0)
     if dataSource is None:
-        print 'Could not open file ' + shpFile
+        print 'Could not open file ' + vrtFile
         sys.exit(1)
     # Get the layer
     layer = dataSource.GetLayer()
@@ -89,31 +115,36 @@ def main(argv=None):
     # Create the heatmap
     ax.imshow(rows, extent=[xmin, xmax, ymin, ymax], interpolation='bilinear')
 
-    # Write the image to a PNG file
+    # Write the image to a temporary PNG file.
+    # This intermediate step is necessary since the AGG backend renderer does
+    # not support the creation of TIFF files.
     tmpFile = "%s.png" % outputFile.rsplit(".", 1)[0]
     plt.savefig(tmpFile)
 
-
+    # Get the GDAL PNG driver and register it
     pngDriver = gdal.GetDriverByName("PNG")
     pngDriver.Register()
 
+    # Open the input dataset
     inDataset = gdal.Open(tmpFile, gdalconst.GA_ReadOnly)
 
-    # Open the TIFF file and append the georeference
+    # Get the GDAL TIFF driver and register it
     tifDriver = gdal.GetDriverByName("GTiff")
     tifDriver.Register()
 
-    # Create the output dataset:
-    # Like the input subdataset the output dataset needs to be 16bit Signed Integer.
-    # The output size is the same as the input size
+    # Copy the input dataset to an output tiff file
     tifDriver.CreateCopy(outputFile, inDataset, 1, ["COMPRESS=LZW", "PREDICTOR=2"])
 
+    # Open the newly created output TIFF file to update the georeference
     outDataset = gdal.Open(outputFile, gdalconst.GA_Update)
+    # Calculate the pixel size in x an y direction
     psx = (xmax - xmin) / outputWidth
     ulx = xmin
     psy = (ymin - ymax) / (outputWidth / 2)
     uly = ymax
+    # Set the geographic transformation
     outDataset.SetGeoTransform([ ulx, psx, 0, uly, 0, psy ])
+    # Set the projection transformation
     outDataset.SetProjection(osr.SRS_WKT_WGS84)
 
     os.remove(tmpFile)
